a53286870b9458b66fb8fedb93d2e6fded30199e
febrian183040121 <wardi.183040121@mail.unpas.ac.id>
2021-07-14 22:46:56 +0700

    penggunaan syntax blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU Blog | Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
+@extends('layouts.main')
 
-</body>
-</html>
+@section('container') 
+
+    <h1>Halaman Posts</h1>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Febrian Wardi",
         "email" => "wardi.183040121@mail.unpas.ac.id",
         "image" => "ava.png"
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('posts');
+Route::get('/posts', function () {
+    return view('posts', [
+        "title" => "Blog"
+    ]);
 });
