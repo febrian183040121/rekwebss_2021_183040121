5dcc5b9010fcc91a2f997e09f42c16f424b4ac01
febrian183040121 <wardi.183040121@mail.unpas.ac.id>
2021-07-24 22:41:55 +0700

    post model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
 
     <article class="mb-5">
         <h2>
-            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
         </h2>
         <p>{{ $post->excerpt }}</p>
     </article>
