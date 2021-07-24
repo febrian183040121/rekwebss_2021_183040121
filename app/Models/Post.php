88654e11feb43e430bed65a026ad4124f5205d20
febrian183040121 <wardi.183040121@mail.unpas.ac.id>
2021-07-24 17:25:52 +0700

    menambahkan DataBase

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,56 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post 
+class Post extends Model
 {
- 
-    private static $blog_posts = [
-            [
-               "title" => "Judul Post Pertama",
-               "slug" => "judul-post-pertama",
-               "author" => "Febrian Wardi",
-               "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-               Non aliquid voluptatem nostrum odio ullam obcaecati aspernatur facilis voluptate 
-               voluptas voluptatum optio debitis libero quae,
-               laborum aut reprehenderit distinctio fugit tenetur vero? Incidunt 
-               hic aspernatur dolore totam officiis odio, quo sit nam? Eum expedita 
-               laboriosam quisquam porro facilis quasi et exercitationem temporibus iure, officiis, 
-               consequuntur voluptatibus quaerat? Ea amet, sed reiciendis quam reprehenderit blanditiis 
-               labore deserunt tempora necessitatibus adipisci, placeat eos, 
-               ex repellat beatae iure consequatur quod distinctio accusantium? Temporibus, amet."
-           ],
-           [
-               "title" => "Judul Post Kedua",
-               "slug" => "judul-post-kedua",
-               "author" => "WARDI",
-               "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-               Non aliquid voluptatem nostrum odio ullam obcaecati aspernatur facilis voluptate 
-               voluptas voluptatum optio debitis libero quae,
-               laborum aut reprehenderit distinctio fugit tenetur vero? Incidunt 
-               hic aspernatur dolore totam officiis odio, quo sit nam? Eum expedita 
-               laboriosam quisquam porro facilis quasi et exercitationem temporibus iure, officiis, 
-               consequuntur voluptatibus quaerat? Ea amet, sed reiciendis quam reprehenderit blanditiis 
-               labore deserunt tempora necessitatibus adipisci, placeat eos, 
-               ex repellat beatae iure consequatur quod distinctio accusantium? Temporibus, amet.
-               laboriosam quisquam porro facilis quasi et exercitationem temporibus iure, officiis, 
-               consequuntur voluptatibus quaerat? Ea amet, sed reiciendis quam reprehenderit blanditiis 
-               labore deserunt tempora necessitatibus adipisci, placeat eos, 
-               ex repellat beatae iure consequatur quod distinctio accusantium? Temporibus, amet."
-           ],
-       ];
+    use HasFactory;
 
-       public static function all()
-       {
-           return collect(self::$blog_posts);
-       }
-
-       public static function find($slug)
-       {
-            $posts = static::all();
-           
-            return $posts->firstWhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
-
